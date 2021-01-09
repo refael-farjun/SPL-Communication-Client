f13431038ce678cf6cc2c9265848985ac607b89c
yanay94sun <yanayshem@gmail.com>
2021-01-10 01:29:26 +0200

    THREADS OK!

--- src/BGRSClient.cpp
+++ src/BGRSClient.cpp
@@ -24,26 +24,26 @@ int main (int argc, char *argv[]) {
     User2client user2client(connectionHandler, mutex);
     Server2Client server2Client(connectionHandler, mutex);
 
-//    std::thread t1(&User2client::run, &user2client);
-//    std::thread t2(&Server2Client::run, &server2Client);
+    std::thread t1(&User2client::run, &user2client);
+    std::thread t2(&Server2Client::run, &server2Client);
 
-    std::thread *myThread1;
-    myThread1 = new std::thread(&User2client::run, &user2client);
+//    std::thread *myThread1;
+//    myThread1 = new std::thread(&User2client::run, &user2client);
+//
+//    std::thread *myThread2;
+//    myThread2 = new std::thread(&Server2Client::run, &server2Client);
+//
+//    myThread1->join();
+//    myThread2->join();
 
-    std::thread *myThread2;
-    myThread2 = new std::thread(&Server2Client::run, &server2Client);
+//    delete myThread1;
+//    delete myThread2;
 
-    myThread1->join();
-    myThread2->join();
+    t2.join();
 
-    delete myThread1;
-    delete myThread2;
+//    exit(0);
 
-//    t2.join();
-
-    exit(0);
-
-//    t1.join();
+    t1.join();
 
 
 
